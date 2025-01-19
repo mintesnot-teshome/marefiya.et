d55c604cdfbc1570f55b9ea79b36b86b01ae1275
Mintesnot Teshome <mintesnotteshome0@gmail.com>
2025-01-19 21:17:19 +0300

    add header, footer, and theme toggle functionality to HTML files

---
+++ views/partials/banner.php
@@ -0,0 +1,16 @@
+<header>
+    <nav>
+        <a href="index.html" class="logo">Marefiya.ET</a>
+        <div class="nav-links">
+            <a href="index.html">Home</a>
+            <a href="hotels.html">Hotels</a>
+            <a href="about.html">About</a>
+            <a href="login.html">Login</a>
+            <a href="register.html">Register</a>
+            <button id="theme-toggle" class="theme-toggle">
+                <img src="https://cdn-icons-png.flaticon.com/512/869/869869.png" class="sun-icon" alt="sun icon">
+                <img src="https://cdn-icons-png.flaticon.com/512/702/702471.png" class="moon-icon" alt="moon icon">
+            </button>
+        </div>
+    </nav>
+</header>
---
+++ views/partials/footer.php
@@ -0,0 +1,24 @@
+<footer>
+    <div class="footer-content">
+        <div class="footer-social">
+            <a href="https://facebook.com" target="_blank" class="social-link">
+                <i class="fab fa-facebook"></i> Facebook
+            </a>
+            <a href="https://twitter.com" target="_blank" class="social-link">
+                <i class="fab fa-twitter"></i> Twitter
+            </a>
+            <a href="https://instagram.com" target="_blank" class="social-link">
+                <i class="fab fa-instagram"></i> Instagram
+            </a>
+            <a href="https://linkedin.com" target="_blank" class="social-link">
+                <i class="fab fa-linkedin"></i> LinkedIn
+            </a>
+        </div>
+        <p>&copy; 2024 Marefiya. All rights reserved.</p>
+    </div>
+</footer>
+<script src="/js/theme.js"></script>
+<script src="/js/script.js"></script>
+</body>
+
+</html>
