4f14466eb69d0f2f6b2060753857225944554ae9
Mintesnot Teshome <mintesnotteshome0@gmail.com>
2025-01-25 16:38:13 +0300

    Refactor theme toggle implementation: update styles, relocate button to footer, and remove unused theme script

--- views/partials/banner.php
+++ views/partials/banner.php
@@ -6,11 +6,6 @@
             <a href="/hotels">Hotels</a>
             <a href="/about">About</a>
             <a href="/login">Login</a>
-            <a href="/register">Register</a>
-            <button id="theme-toggle" class="theme-toggle">
-                <img src="https://cdn-icons-png.flaticon.com/512/869/869869.png" class="sun-icon" alt="sun icon">
-                <img src="https://cdn-icons-png.flaticon.com/512/702/702471.png" class="moon-icon" alt="moon icon">
-            </button>
         </div>
     </nav>
 </header>
