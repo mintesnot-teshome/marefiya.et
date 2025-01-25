b174c2788eb5bec8c4828abbcab14b1529567881
Mintesnot Teshome <mintesnotteshome0@gmail.com>
2025-01-25 14:26:25 +0300

    Refactor function.php to remove unused dd() function and clean up code

--- function.php
+++ function.php
@@ -4,12 +4,4 @@
 function urlIs($value)
 {
     return $_SERVER['REQUEST_URI'] === $value;
-}
-
-function dd($value)
-{
-    echo "<pre>";
-    var_dump($value);
-    echo "</pre>";
-    die();
-}
+};
