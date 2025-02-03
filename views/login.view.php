af9e3c34337f0735c62a6fe584c15cf9ac9f41bf
abreham777 <abrehamlast777@gmail.com>
2025-02-03 23:42:22 +0300

    Update login.view.php

--- views/login.view.php
+++ views/login.view.php
@@ -14,7 +14,7 @@
             </div>
             <button type="submit" class="auth-btn">Login</button>
         </form>
-        <p>Don't have an account? <a href="/register">Register here</a></p>
+        <p>Don't have an account? <a href="/register">Register Here</a></p>
     </div>
 </main>
 <?php require('partials/footer.php') ?>
